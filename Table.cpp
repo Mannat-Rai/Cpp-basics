3a521691dc08642990da5958d93c064e962b2197
Mannat Rai <raim81144@gmail.com>
2024-06-19 09:28:46 +0530

    Prints the multiplication table of 12.

---
+++ Table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Program to print multiplication table of 12 , using for loop.
+
+    int i;
+    for(i=12;i<=120;i=i+12){
+        cout<<i<<endl;
+    }
+    return 0;
+}
