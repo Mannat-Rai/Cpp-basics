5aaf9d7d3f4cdb35b6071a9831680db9034504fc
Mannat Rai <raim81144@gmail.com>
2024-06-19 08:54:32 +0530

    Prints even no, from 1 to 100.

---
+++ even.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Program to find even numbers from 1 to 100.
+    int i = 2;
+    while(i<=100){
+        cout<<i<<endl;
+        i = i + 2;
+    }
+    return 0;
+}
