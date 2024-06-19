518fc9c1cf3c62f1b4186282935a84718c01ade5
Mannat Rai <raim81144@gmail.com>
2024-06-19 08:53:40 +0530

    prints  Character from a to z.

---
+++ Character.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Program to print all characters from a to z using while loop.
+    char alpha = 'a';
+    while(alpha <= 'z'){
+        cout<<alpha<<endl;
+        alpha++;
+    }
+    return 0;
+}
