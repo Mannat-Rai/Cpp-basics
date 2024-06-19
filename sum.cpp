5ef7f35d2a2e204a73a9354ee4b2904ac98b2129
Mannat Rai <raim81144@gmail.com>
2024-06-19 08:49:28 +0530

    Calculates  sum from 1 to 10.

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Program to find sum of numbers from 1 to 10.
+    int i = 1;
+    int sum=0;
+    while(i<=10){
+    sum =i+sum;
+     i++;}
+     
+     cout<<sum<<endl;
+
+    
+    return 0;
+}
