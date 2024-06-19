de98e318b40f9f3f62051960323884b6f738160a
Mannat Rai <raim81144@gmail.com>
2024-06-19 08:52:21 +0530

    Prints natural no. in reverse from 1 to 10.

---
+++ reverse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //To print natural numbers in reverse order from 10 to 1.
+    int i = 10;
+    while(i>=1){
+        cout<<i<<endl;
+        --i;
+    }
+    return 0;
+}
